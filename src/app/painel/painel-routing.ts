197052953aafb6c60198b9e3dbaac2c596beff93
Yuri Batista <yuri.batista@stela.org.br>
2021-03-01 09:42:41 -0300

    navegacao

--- src/app/painel/painel-routing.ts
+++ src/app/painel/painel-routing.ts
@@ -1,11 +1,16 @@
 import { Routes } from '@angular/router';
 import { PainelGerenteComponent } from './painel-gerente/painel-gerente.component';
 import { PainelClienteComponent } from './painel-cliente/painel-cliente.component';
+import { PainelComponent } from './painel.component';
 
 export const PainelRouter: Routes = [
     {
         path: '',
         children: [
+            {
+                path: '',
+                component: PainelComponent,
+            },
             {
                 path: 'gerente',
                 component: PainelGerenteComponent,
